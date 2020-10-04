5346c9411933715b6acfdb43364cf36d9397efcb
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2020-10-05 04:49:42 +0600

    #Home Screen setup

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, Image, TouchableOpacity } from "react-native";
+
+
+const HomeScreen = (props) => {
+    console.log(props)
+    return (
+        <View>
+            <Text style={styles.textStyle}>Home</Text>
+
+        </View>
+        
+        );
+
+};
+
+const styles = StyleSheet.create({
+
+
+});
